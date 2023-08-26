15661b55b2a3ee6f7e2d93ded72c540c484f598f
mames1dev <kamakuraowner@outlook.jp>
2023-08-26 15:16:22 +0900

    上げ忘れ

---
+++ EditForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace garushisu_patcher
+{
+    partial class EditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditForm));
+            this.lblPreset = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.cmbPreset = new System.Windows.Forms.ComboBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPreset
+            // 
+            this.lblPreset.AutoSize = true;
+            this.lblPreset.BackColor = System.Drawing.Color.Transparent;
+            this.lblPreset.Font = new System.Drawing.Font("Kristen ITC", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPreset.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPreset.Location = new System.Drawing.Point(12, 29);
+            this.lblPreset.Name = "lblPreset";
+            this.lblPreset.Size = new System.Drawing.Size(205, 28);
+            this.lblPreset.TabIndex = 3;
+            this.lblPreset.Text = "Delete Your Preset!";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblName.Location = new System.Drawing.Point(163, 86);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(81, 17);
+            this.lblName.TabIndex = 13;
+            this.lblName.Text = "Preset name";
+            // 
+            // cmbPreset
+            // 
+            this.cmbPreset.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbPreset.FormattingEnabled = true;
+            this.cmbPreset.Location = new System.Drawing.Point(145, 115);
+            this.cmbPreset.Name = "cmbPreset";
+            this.cmbPreset.Size = new System.Drawing.Size(121, 31);
+            this.cmbPreset.TabIndex = 14;
+            this.cmbPreset.Text = "Presets";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(166, 166);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete!";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // EditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(404, 201);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.cmbPreset);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblPreset);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EditForm";
+            this.Text = "Delete Preset";
+            this.Load += new System.EventHandler(this.EditForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPreset;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.ComboBox cmbPreset;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ EditForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace garushisu_patcher
+{
+    public partial class EditForm : Form
+    {
+        public EditForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(cmbPreset.SelectedIndex == -1)
+            {
+                MessageBox.Show("Preset not specified!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            String sqlQuery = "DELETE FROM [setting] where [name] = '" + cmbPreset.Text + "'";
+
+            OleDbCommand command = new OleDbCommand();
+            OleDbConnection cnAccess = new OleDbConnection();
+            cnAccess.ConnectionString = Properties.Settings.Default.settingConnection;
+
+            try
+            {
+                cnAccess.Open();
+                command.Connection = cnAccess;
+                command.CommandText = sqlQuery.ToString();
+                command.ExecuteNonQuery();
+
+            }
+            catch
+            {
+                String message = "Failed to delete preset. The Microsoft Access database engine 2010 redistributable component required for delete may not be installed. Do you want to install it?";
+
+                DialogResult dr = MessageBox.Show(message,
+                    "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (dr == DialogResult.Yes)
+                {
+
+                    ProcessStartInfo pi = new ProcessStartInfo()
+                    {
+                        FileName = "https://www.microsoft.com/en-us/download/details.aspx?id=13255",
+                        UseShellExecute = true,
+                    };
+
+                    Process.Start(pi);
+
+                    MainForm.Instance.Close();
+                }
+
+                return;
+            }
+            finally
+            {
+                command.Dispose();
+                cnAccess.Close();
+            }
+
+            MessageBox.Show("Successful to delete preset!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            LoadForm.LoadFormInstance.Close();
+            Close();
+        }
+
+        private void EditForm_Load(object sender, EventArgs e)
+        {
+            String sqlQuery = "select [name] from [setting]";
+
+            DataTable dt = new DataTable();
+            OleDbCommand command = new OleDbCommand();
+            OleDbDataAdapter da = new OleDbDataAdapter();
+            OleDbConnection cnAccess = new OleDbConnection();
+            cnAccess.ConnectionString = Properties.Settings.Default.settingConnection;
+
+            try
+            {
+                cnAccess.Open();
+                command.Connection = cnAccess;
+                command.CommandText = sqlQuery;
+                da.SelectCommand = command;
+
+                da.Fill(dt);
+            }
+            catch
+            {
+                String message = "Failed to load configuration. The Microsoft Access database engine 2010 redistributable component required for loading may not be installed. Do you want to install it?";
+
+                DialogResult dr = MessageBox.Show(message,
+                    "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (dr == DialogResult.Yes)
+                {
+
+                    ProcessStartInfo pi = new ProcessStartInfo()
+                    {
+                        FileName = "https://www.microsoft.com/en-us/download/details.aspx?id=13255",
+                        UseShellExecute = true,
+                    };
+
+                    Process.Start(pi);
+
+                    Close();
+
+                    return;
+
+                }
+            }
+            finally
+            {
+                command.Dispose();
+                da.Dispose();
+                cnAccess.Close();
+            }
+            
+            foreach (DataRow dr in dt.Rows)
+            {
+                cmbPreset.Items.Add(dr.ItemArray[0]);
+            }
+        }
+    }
+}
